4a7d70561741346364eeb080a949d763d94ee70e
ShiningLea <49339966+AnErrupTion@users.noreply.github.com>
2021-05-07 20:27:55 +0200

    v1.1

--- src/open/nano/proxy/Main.java
+++ src/open/nano/proxy/Main.java
@@ -1,6 +1,5 @@
 package open.nano.proxy;
 
-import open.java.toolkit.Arrays;
 import open.java.toolkit.Regex;
 import open.java.toolkit.System;
 import open.java.toolkit.console.Console;
@@ -11,45 +10,78 @@ import open.java.toolkit.files.Files;
 import open.java.toolkit.http.Request;
 import open.java.toolkit.threading.Parallel;
 
-import java.io.*;
+import java.io.IOException;
 import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.ProxySelector;
-import java.net.http.*;
+import java.net.http.HttpClient;
+import java.net.http.HttpResponse;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main
 {
-    public static void main(String[] args) throws IOException, InterruptedException, NoSuchAlgorithmException
+    public static void main(String[] args) throws NoSuchAlgorithmException
     {
-        Console.setTitle("Nano Proxy");
-        Request.setVersion(HttpClient.Version.HTTP_1_1);
+        Console.setTitle("Nano Proxy v1.1 | JavaToolkit v" + System.getToolkitVersion());
+        FileParser parser = new FileParser("config.txt", "#", ":", true, 1);
 
-        Console.writeLine("Scrape proxies from sources? (true/false)", LogType.INFO);
-        boolean scraping = Boolean.parseBoolean(Console.readLine());
+        int timeout = parser.getInt("timeout");
+        boolean debugging = parser.getBoolean("debugging");
+        boolean lessAccuracy = parser.getBoolean("lessAccuracy");
+        boolean showBad = parser.getBoolean("showBad");
+        boolean showUnknown = parser.getBoolean("showUnknown");
+        boolean scrapeLinks = parser.getBoolean("scrapeLinks");
+        boolean scrapeProxies = parser.getBoolean("scrapeProxies");
+        boolean capture = parser.getBoolean("capture");
 
-        if (scraping)
+        System.showErrors = debugging;
+        System.logErrors = debugging;
+
+        Request.setTimeout(timeout);
+        Request.getBuilder().followRedirects(HttpClient.Redirect.ALWAYS);
+        Request.getBuilder().version(HttpClient.Version.HTTP_1_1);
+
+        if (scrapeLinks)
         {
-            if (!Files.fileExists("proxies.txt"))
-                Files.createFile("proxies.txt");
+            Console.setTitle("Nano Proxy | Scraping links...");
+            String[] keywords = Files.readLines("keywords.txt");
+            AtomicInteger words = new AtomicInteger();
 
-            Parallel.forEach(Files.readFile("sources.txt").split("\n"), source ->
+            Parallel.forEach(keywords, keyword ->
             {
-                Console.ansiWriteLine(Foreground.YELLOW, "Scraping from source : " + source, LogType.INFO);
+                Console.setTitle("Nano Proxy | Scraping links | " + words + " / " + keywords.length + " Keywords");
 
-                String body = Request.sendGet(source).body();
-                ArrayList<String> matches = Regex.getMatches(body, "(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})(?=[^\\d])\\s*:?\\s*(\\d{2,5})");
+                String response = (String) Request.send("https://www.bing.com/search?q=" + keyword.replace(" ", "+"), "get", null).body();
+                ArrayList<String> matches = Regex.getMatches(response, "((http|https)://)(www.)?[a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)");
 
-                Files.writeLines("proxies.txt", Arrays.toStringArray(matches.toArray()), true);
-                Console.ansiWriteLine(Foreground.YELLOW, "Scraped " + matches.size() + " proxies from source : " + source, LogType.INFO);
+                int size = matches.size();
+                if (size > 0)
+                {
+                    Console.ansiWriteLine(Foreground.GREEN, "Found " + size + " links. (" + keyword + ")", LogType.INFO);
+                    Files.writeLines("sources.txt", matches.stream().filter(link -> !link.contains("aclick?ld=")).toArray(String[]::new), true);
+                }
+
+                words.getAndIncrement();
             });
         }
 
-        if (!Files.fileExists("live.txt"))
-            Files.createFile("live.txt");
+        if (scrapeProxies)
+        {
+            Console.setTitle("Nano Proxy | Scraping proxies...");
+
+            Parallel.forEach(Files.readLines("sources.txt"), source ->
+            {
+                Console.ansiWriteLine(Foreground.YELLOW, "Scraping from source : " + source, LogType.INFO);
+
+                String body = (String) Request.send(source, "get", null).body();
+                ArrayList<String> matches = Regex.getMatches(body, "(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})(?=[^\\d])\\s*:?\\s*(\\d{2,5})");
+
+                Files.writeLines("proxies.txt", matches.toArray(String[]::new), true);
+                Console.ansiWriteLine(Foreground.YELLOW, "Scraped " + matches.size() + " proxies from source : " + source, LogType.INFO);
+            });
+        }
 
         Console.writeLine("Amount of threads :", LogType.INFO);
         Parallel.threads = Integer.parseInt(Console.readLine());
@@ -57,69 +89,184 @@ public class Main
         Console.setTitle("Nano Proxy | Loading...");
 
         String[] proxies = Files.readLines("proxies.txt");
-        ArrayList<String> list = Arrays.toArrayList(proxies);
-        Collections.shuffle(list);
-        proxies = list.toArray(new String[list.size()]);
-
         String[] judges = Files.readLines("judges.txt");
+
         SecureRandom random = SecureRandom.getInstanceStrong();
-        FileParser parser = new FileParser("config.txt", "#", ":", true, 1);
         AtomicInteger checked = new AtomicInteger(), live = new AtomicInteger();
 
-        boolean fastChecking = parser.parseBoolean("fastChecking");
-        int timeout = parser.parseInt("timeout");
-        boolean debugging = parser.parseBoolean("debugging");
-        boolean lessAccuracy = parser.parseBoolean("lessAccuracy");
+        String[] l2Headers = new String[]
+                {
+                        "http_ACCPROXYWS",
+                        "http_Cdn_Src_Ip",
+                        "http_Client_IP",
+                        "http_CUDA_CLIIP",
+                        "http_Forwarded",
+                        "http_Forwarded_For",
+                        "http_REMOTE_HOST",
+                        "http_X_Client_Ip",
+                        "http_X_Coming_From",
+                        "http_X_Forwarded",
+                        "http_X_Forwarded_For",
+                        "http_X_Forwarded_For_IP",
+                        "http_X_Forwarded_Host",
+                        "http_X_Forwarded_Server",
+                        "http_X_Host",
+                        "http_X_Network_Info",
+                        "http_X_Nokia_RemoteSocket",
+                        "http_X_ProxyUser_IP",
+                        "http_X_QIHOO_IP",
+                        "http_X_Real_IP",
+                        "http_XCnool_forwarded_for",
+                        "http_XCnool_remote_addr"
+                };
 
-        Request.setTimeout(timeout);
+        String[] l4Headers = new String[]
+                {
+                        "http_Mt_Proxy_ID",
+                        "http_Proxy_agent",
+                        "http_Proxy_Connection",
+                        "http_Surrogate_Capability",
+                        "http_Via",
+                        "http_X_Accept_Encoding",
+                        "http_X_ARR_LOG_ID",
+                        "http_X_Authenticated_User",
+                        "http_X_BlueCoat_Via",
+                        "http_X_Cache",
+                        "http_X_CID_HASH",
+                        "http_X_Content_Opt",
+                        "http_X_D_Forwarder",
+                        "http_X_Fikker",
+                        "http_X_Forwarded_Port",
+                        "http_X_Forwarded_Proto",
+                        "http_X_IMForwards",
+                        "http_X_Loop_Control",
+                        "http_X_MATO_PARAM",
+                        "http_X_NAI_ID",
+                        "http_X_Nokia_Gateway_Id",
+                        "http_X_Nokia_LocalSocket",
+                        "http_X_Original_URL",
+                        "http_X_Proxy_ID",
+                        "http_X_Roaming",
+                        "http_x_teamsite_preremap",
+                        "http_X_Tinyproxy",
+                        "http_X_TurboPage",
+                        "http_X_Varnish",
+                        "http_X_Via",
+                        "http_X_WAP_Profile",
+                        "http_X_WrProxy_ID",
+                        "http_http_X_XFF_0",
+                        "http_Xroxy_Connection"
+                };
 
-        String[] finalProxies = proxies;
+        AtomicInteger cpm = new AtomicInteger();
+        AtomicInteger clock = new AtomicInteger();
+
+        Thread timer = new Thread(() ->
+        {
+            while (true)
+            {
+                clock.getAndIncrement();
+                try
+                {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ignored) {}
+            }
+        });
+
+        Thread thread = new Thread(() ->
+        {
+            while (true)
+            {
+                long total = Runtime.getRuntime().totalMemory();
+                long free = Runtime.getRuntime().freeMemory();
+                long used = total - free;
+
+                cpm.set(checked.get() * 60 / clock.get());
+
+                Console.setTitle("Nano Proxy | Memory : " + (used / 1024 / 1024) + " / " + (total / 1024 / 1024) + " MB | Checking (" + checked + "/" + proxies.length + ") | " + live + " Live | " + cpm + " CPM");
+
+                try
+                {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ignored) {}
+            }
+        });
+
+        timer.start();
+        try
+        {
+            Thread.sleep(1100);
+        } catch (InterruptedException ignored) {}
+        thread.start();
+
         Parallel.forEach(proxies, proxy ->
         {
-            String judge = null;
+            Request.setProxy(proxy);
 
+            String judge = judges[random.nextInt(judges.length)];
             try
             {
-                Console.setTitle("Nano Proxy | Checking (" + checked + "/" + finalProxies.length + ") | " + live + " Live");
+                long start = java.lang.System.nanoTime();
+                boolean reachable = InetAddress.getByName(proxy.split(":")[0]).isReachable(timeout);
+                long finish = java.lang.System.nanoTime();
 
-                String[] array = proxy.split(":");
-                boolean valid;
-                int statusCode = 0;
-
-                if (fastChecking)
-                    valid = InetAddress.getByName(array[0]).isReachable(timeout);
-                else
+                if (reachable)
                 {
-                    Request.setProxy(ProxySelector.of(new InetSocketAddress(array[0], Integer.parseInt(array[1]))));
+                    HttpResponse<String> response = (HttpResponse<String>) Request.send(judge, "get", null);
+                    int status = response.statusCode();
 
-                    judge = judges[random.nextInt(judges.length)];
-                    HttpResponse<String> response = Request.sendGet(judge);
-
-                    statusCode = response.statusCode();
-                    valid = statusCode == 200;
-                }
-
-                if (valid)
-                {
-                    Console.ansiWriteLine(Foreground.GREEN, "[GOOD]    " + proxy, LogType.INFO);
-                    Files.writeString("live.txt", proxy + System.newLine, true);
-
-                    live.getAndIncrement();
-                }
-                else
-                {
-                    if (lessAccuracy && (statusCode == 302 || statusCode == 501 || statusCode == 503))
+                    if (!lessAccuracy ? status == 200 : (status == 200 || status == 302 || status == 501 || status == 503) && !System.errorOccurred)
                     {
-                        Console.ansiWriteLine(Foreground.GREEN, "[GOOD]    " + proxy, LogType.INFO);
-                        Files.writeString("live.txt", proxy + System.newLine, true);
+                        String ping = TimeUnit.MILLISECONDS.convert(finish - start, TimeUnit.NANOSECONDS) + "ms";
+                        String anonymity = "Unknown";
+
+                        if (capture)
+                        {
+                            String azenv = (String) Request.send("http://azenv.net/", "get", null).body();
+
+                            for (String l2 : l2Headers)
+                                if (azenv.contains(l2.toUpperCase()))
+                                {
+                                    anonymity = "Transparent";
+                                    break;
+                                }
+
+                            if (!anonymity.equals("Unknown"))
+                                for (String l4 : l4Headers)
+                                    if (azenv.contains(l4.toUpperCase()))
+                                    {
+                                        anonymity = "Anonymous";
+                                        break;
+                                    }
+
+                            if (!anonymity.equals("Unknown")) anonymity = "Elite";
+                        }
+
+                        String str = proxy + (capture ? " | " + ping + " | " + anonymity : "");
+
+                        Console.ansiWriteLine(Foreground.GREEN, "[GOOD]    " + str, LogType.INFO);
+                        Files.writeString("live.txt", str + System.newLine, true);
 
                         live.getAndIncrement();
-                    } else Console.ansiWriteLine(Foreground.RED, "[BAD]     " + proxy + (debugging && !fastChecking ? " - Status code : " + statusCode : ""), LogType.INFO);
+                    }
+                    else
+                    {
+                        System.errorOccurred = false;
+
+                        if (showBad)
+                            Console.ansiWriteLine(Foreground.RED, "[BAD]     " + proxy + (debugging ? " - Status code : " + status + " (" + judge + ")" : ""), LogType.INFO);
+                    }
+                }
+                else
+                {
+                    if (showBad)
+                        Console.ansiWriteLine(Foreground.RED, "[BAD]     " + proxy + (debugging ? " - Reachable : " + reachable + " (" + judge + ")" : ""), LogType.INFO);
                 }
             }
-            catch (Throwable t)
+            catch (IOException ex)
             {
-                Console.ansiWriteLine(Foreground.BLUE, "[UNKNOWN] " + (debugging && !fastChecking ? "(" + judge + ") " : "") + proxy + (debugging ? " - " + t.getMessage() : ""), LogType.INFO);
+                if (showUnknown)
+                    Console.ansiWriteLine(Foreground.BLUE, "[UNKNOWN] " + (debugging ? "(" + judge + ") " : "") + proxy + (debugging ? " - " + ex.getMessage() : ""), LogType.INFO);
             }
 
             checked.getAndIncrement();
@@ -129,7 +276,5 @@ public class Main
 
         Console.writeLine("Press Enter key to exit program...", LogType.INFO);
         Console.readLine();
-
-        java.lang.System.exit(0);
     }
 }
